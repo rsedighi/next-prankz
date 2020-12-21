0382ec0d89dcc9066bb364ec4ed7ab50cfa78d85
Ramin <ramin@Ramins-MacBook-Pro.local>
2020-12-20 16:12:38 -0800

    some header info changed

--- components/PrankHead.js
+++ components/PrankHead.js
@@ -6,16 +6,16 @@ export default function PrankHead({ title }) {
       <title>{title}</title>
       <link rel="icon" href="/favicon.jpg" />
       <meta property="og:title" content={title} />
-      <meta property="og:url" content="https://chicag0tribune.netlify.app" />
+      <meta property="og:url" content="https://nbarumors.netlify.app" />
       <meta property="og:image" content="/icon.png" />
-      <meta property="og:description" content="See the latest news from the Chicag0 Tribune" />
-      <meta property="og:site_name" content="Chicag0 Tribune" />
+      <meta property="og:description" content="See the latest news from the NBA on ESPN" />
+      <meta property="og:site_name" content="ESPN" />
       <meta name="monetization" content="$ilp.uphold.com/rEgHHBQGDUDP" />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content="See the latest news from the Chicag0 Tribune." />
+      <meta name="twitter:description" content="See the latest news from the NBA on ESPN." />
       <meta name="twitter:creator" content="@cassidoo" />
-      <meta name="twitter:image" content="https://chicag0tribune.netlify.app/icon.png" />
+      <meta name="twitter:image" content="https://nbarumors.netlify.app/icon.png" />
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     </Head>
   )
